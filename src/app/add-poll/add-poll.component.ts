7372836a752bef3acef807a95a6b6fa2c90562ca
Samuel Francis Waweru Ndegwa <s.f.waweru@gmail.com>
2024-07-10 16:56:18 +0300

    Added Poll Form

--- src/app/add-poll/add-poll.component.ts
+++ src/app/add-poll/add-poll.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-poll',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './add-poll.component.html',
   styleUrl: './add-poll.component.css'
 })
-export class AddPollComponent {
+export class AddPollComponent implements OnInit {
+  form!:FormGroup
+  constructor(
+    private fb:FormBuilder
+  ){}
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      Question: this.fb.control(null, Validators.required),
+      Choices : this.fb.array([])
+    })
+  }
+
+
+  get choices():FormArray {
+    return this.form.get('Choices') as FormArray;
+  }
+  addChoice(){
+    this.choices.push(new FormControl('', Validators.required))
+  }
+  onSubmit(){
+    
+  }
 }
